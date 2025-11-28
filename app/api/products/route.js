dae3b9ab9ac477bb3882265662dce324d8041a58
Rishabh Singh Parihar <your-email@example.com>
2025-11-28 12:20:56 +0530

    add new changes

---
+++ app/api/products/route.js
@@ -0,0 +1,7 @@
+export async function POST(req) {
+  const body = await req.json();
+  return Response.json({
+    status: "success",
+    data: body
+  });
+}
---
+++ app/userprofile/page.tsx
@@ -0,0 +1,25 @@
+import {
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+
+export default async function Page() {
+  const res = await fetch("http://localhost:5000/api/users", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    name: "Rishabh",
+    email: "rishabh@gmail.com"
+  })
+});
+
+  const backendData = await res.json();
+
+  return (
+    <SidebarProvider>
+      <div>This thing is working fine</div>
+
+      <pre>{JSON.stringify(backendData, null, 2)}</pre>
+    </SidebarProvider>
+  );
+}
+
